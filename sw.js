f972fa7c1779c083bff79307fc11ac805e00b841
Hoook21 <141882724+Hoook21@users.noreply.github.com>
2026-06-11 18:32:18 +0200

    Smooth training load curves

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'athlete-os-v2';
+const CACHE = 'athlete-os-v3';
 const ASSETS = ['./', './index.html', './styles.css', './chart.css', './app.js', './manifest.webmanifest'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
